01db7309958b148a60e3c7e022485fdcca841c1b
Michael Humphries <michaelhumphries1997@gmail.com>
2020-02-07 09:28:50 +0000

    automated update

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ORIGIN=$(cat .git/config | grep url | awk '{print $3}')
-read -p "do you want to push to $ORIGIN?" option
+read -p $'do you want to push to $ORIGIN?\n\"yes\" || \"no\"\n' option
 if [ $option == "yes" ]; then
     echo "pushing profiles to $ORIGIN"
     git add .
